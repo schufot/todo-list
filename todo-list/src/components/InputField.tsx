790251bce6d694895b7f0e479f407e8fb80f81ac
schufot <156847189+schufot@users.noreply.github.com>
2024-08-05 10:59:47 +0200

    Add styles

---
+++ todo-list/src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const InputField = () => {
+  return (
+    <form className="input">
+      <input type="input" placeholder="Enter a task" className="input__box" />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
